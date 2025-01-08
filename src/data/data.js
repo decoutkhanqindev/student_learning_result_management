b07d91a109e94126a5116e329be854a6aca9aa37
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-01-08 14:00:03 +0700

    add fake data json

---
+++ src/data/data.js
@@ -0,0 +1,45 @@
+[
+  {
+    "_id": "s01",
+    "name": "Nguyen Van A",
+    "subjects": [
+      {
+        "id": "sj01",
+        "name": "Math",
+        "avg": 8.5
+      },
+      {
+        "id": "sj02",
+        "name": "Physics",
+        "avg": 7.0
+      }
+    ]
+  },
+  {
+    "_id": "s02",
+    "name": "Le Thi B",
+    "subjects": [
+      {
+        "id": "sj03",
+        "name": "Chemistry",
+        "avg": 9.0
+      },
+      {
+        "id": "sj04",
+        "name": "Biology",
+        "avg": 6.5
+      }
+    ]
+  },
+  {
+    "_id": "s03",
+    "name": "Tran Van C",
+    "subjects": [
+      {
+        "id": "sj05",
+        "name": "English",
+        "avg": 7.5
+      }
+    ]
+  }
+]

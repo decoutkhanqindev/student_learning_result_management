8a6630953e6e37d8f32f988a59eb29117b541278
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-01-09 14:13:17 +0700

    add router to use in app

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
-const dotenv = require("dotenv");
+require("dotenv").config();
+require("./config/mongodb"); // connect db
 const express = require("express");
 const bodyParser = require("body-parser");
-const connectDb = require("./config/mongodb")
+const router = require("./routes/router");
 
 const app = express();
-dotenv.config();
 const env = process.env;
 const PORT = env.PORT || 8080;
-const MONGO_DB_URI = env.MONGO_DB_URI
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-connectDb(MONGO_DB_URI);
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`>>> Server is running on port ${PORT}.`);

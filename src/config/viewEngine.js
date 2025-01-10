316a2bf48911bff048d007e51d6ccb6da7d6d0e5
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-01-11 00:29:46 +0700

    update project

--- src/config/viewEngine.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const path = require("path");
-
-const config = (app) => {
-  app.set("view engine", "ejs");
-  app.set("views", path.join("./src", "views"));
-};
-
-module.exports = config;

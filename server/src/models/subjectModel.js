aa12f6d72d4c22bdc2637edeb20c1c4217f75503
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-01-11 00:29:30 +0700

    add server side

---
+++ server/src/models/subjectModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const subjectSchema = new Schema({
+  _id: { type: String, required: true },
+  subjectName: { type: String, required: true },
+  teacher: { type: String, required: true }
+});
+
+const Subject = mongoose.model("Subject", subjectSchema);
+module.exports = Subject;

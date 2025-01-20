1f097dec4aa50e24b987b795584a0959b4f59000
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-01-20 14:04:07 +0700

    update all models

--- src/server/models/studentModel.js
+++ src/server/models/studentModel.js
@@ -1,16 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const subjectRefSchema = new Schema({
-  _id: { type: String, ref: "Subject" } // reference to Subject model
-});
-
-const studentSchema = new Schema({
-  _id: { type: String, required: true },
-  fullName: { type: String, required: true },
-  className: { type: String, required: true },
-  subjects: [subjectRefSchema]
-});
+const studentSchema = new Schema(
+  {
+    _id: { type: String, required: true },
+    fullName: { type: String, required: true },
+    className: { type: String, required: true },
+    subjects: [{ type: String, ref: "Subject" }] // reference to Subject model
+  },
+  { versionKey: false }
+);
 
 const Student = mongoose.model("Student", studentSchema);
 module.exports = Student;
--- src/server/models/subjectModel.js
+++ src/server/models/subjectModel.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const subjectSchema = new Schema({
-  _id: { type: String, required: true },
-  subjectName: { type: String, required: true },
-  teacher: { type: String, required: true }
-});
+const subjectSchema = new Schema(
+  {
+    _id: { type: String, required: true },
+    subjectName: { type: String, required: true },
+    teacher: { type: String, required: true },
+    students: [{ type: String, ref: "Student" }] // reference to Student model
+  },
+  { versionKey: false }
+);
 
 const Subject = mongoose.model("Subject", subjectSchema);
 module.exports = Subject;

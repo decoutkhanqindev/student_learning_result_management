2a9e889a1ff92a17554d60d1de3a2a16be7f04be
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-01-21 13:08:39 +0700

    update student service

--- src/server/services/studentService.js
+++ src/server/services/studentService.js
@@ -4,74 +4,56 @@ class StudentService {
   static async getAllStudentsService() {
     try {
       console.log("\n>>> getAllStudentsService is called.");
-      const students = await Student.find().select("-subjects").select("-__v");
+      const students = await Student.find();
       return students;
     } catch (error) {
-      console.log("\n>>> getAllStudentsService have error: " + error.message);
+      console.log("\n>>> getAllStudentsService have error: ", error.message);
       throw error;
     }
   }
 
-  static async getStudentByIdService(_id) {
+  static async getStudentByIdService(id) {
     try {
       console.log("\n>>> getStudentByIdService is called.");
-      const student = await Student.findById(_id).select("-__v");
+      const student = await Student.findById({ _id: id });
       return student;
     } catch (error) {
-      console.log("\n>>> getStudentByIdService have error: " + error.message);
+      console.log("\n>>> getStudentByIdService have error: ", error.message);
       throw error;
     }
   }
 
-  static async getAllSubjectIdsByIdService(_id) {
+  static async autoGenerateStudentId() {
     try {
-      console.log("\n>>> getAllSubjectIdsByIdService is called.");
-      const student = await this.getStudentByIdService(_id);
-      const subjectIds = student.subjects.map((subject) => subject._id);
-      return subjectIds;
+      console.log("\n>>> autoGenerateStudentId is called.");
+      const lastStudent = await Student.findOne().sort({ _id: -1 });
+      if (!lastStudent) return "S001";
+      const lastId = lastStudent._id;
+      // sampled id is "S001"
+      // lastId.substring(1) => "001"
+      // parseInt(lastId.substring(1), 10) => 1 (base 10)
+      const lastIdNumber = parseInt(lastId.substring(1), 10);
+      const newIdNumber = lastIdNumber + 1;
+      // sampled id number = 15
+      // newIdNumber.toString() => "15"
+      // padStart(3, "0") => "0015"
+      const newStudentId = `S${newIdNumber.toString().padStart(3, "0")}`;
+      return newStudentId;
     } catch (error) {
-      console.log(
-        "\n>>> getAllSubjectIdsByIdService have error: " + error.message
-      );
+      console.log("\n>>> autoGenerateStudentId have error: ", error.message);
       throw error;
     }
   }
 
-  static async addStudentService(_id, fullName, className, subjects) {
+  static async addStudentService(fullName, className, subjects) {
     try {
       console.log("\n>>> addStudentService is called.");
+      const _id = await StudentService.autoGenerateStudentId();
       const newStudent = new Student({ _id, fullName, className, subjects });
-      return await newStudent.save();
+      const addedStudent = await newStudent.save();
+      return addedStudent;
     } catch (error) {
       console.log("\n>>> addStudentService have error: ", error.message);
-      throw error;
-    }
-  }
-
-  static async updateStudentByIdService(_id, fullName, className, subjects) {
-    try {
-      console.log("\n>>> updateStudentService is called.");
-      const updatedStudent = await Student.findByIdAndUpdate(
-        _id,
-        { fullName, className, subjects },
-        { new: true } // return updated document
-      ).select("-__v");
-      return updatedStudent;
-    } catch (error) {
-      console.log("\n>>> updateStudentService have error: ", error.message);
-      throw error;
-    }
-  }
-
-  static async deleteStudentByIdService(_id) {
-    try {
-      console.log("\n>>> deleteStudentByIdService is called.");
-      const deletedStudent = await Student.findByIdAndDelete(_id).select(
-        "-__v"
-      );
-      return deletedStudent;
-    } catch (error) {
-      console.log("\n>>> deleteStudentByIdService have error: ", error.message);
       throw error;
     }
   }

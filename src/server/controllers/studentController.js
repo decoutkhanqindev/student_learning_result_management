e35dee816e0714c07f85a51cdeda08d58f6ed9b3
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-01-21 13:09:02 +0700

    update student controller

--- src/server/controllers/studentController.js
+++ src/server/controllers/studentController.js
@@ -1,111 +1,55 @@
 const studentService = require("../services/studentService");
 
 class StudentController {
-  static async getAllStudentController(req, res) {
+  static async getAllStudentsController(req, res) {
     try {
-      console.log("\n>>> getAllStudentController is called.");
+      console.log("\n>>> getAllStudentsController is called.");
       const students = await studentService.getAllStudentsService();
-      if (!students.length) {
+      if (!students) {
         res.status(404).json({ message: "No student found here." });
-      } else {
-        res.status(200).json(students);
       }
+      res.status(200).json(students);
     } catch (error) {
-      console.log("\n>>> getAllStudentController have error: " + error.message);
-      res.status(500).json({ message: error.message });
+      console.log(
+        "\n>>> getAllStudentsController have error: " + error.message
+      );
+      res.status(500).json({ message: "Server have error." });
     }
   }
 
   static async getStudentByIdController(req, res) {
     try {
       console.log("\n>>> getStudentByIdController is called.");
-      const _id = req.params.id;
-      const student = await studentService.getStudentByIdService(_id);
+      const { id } = req.params;
+      const student = await studentService.getStudentByIdService(id);
       if (!student) {
-        res.status(404).json({ message: `No student with ${_id} found.` });
-      } else {
-        res.status(200).json(student);
+        res.status(404).json({ message: `Not found student with ${id} here.` });
       }
+      res.status(200).json(student);
     } catch (error) {
       console.log(
         "\n>>> getStudentByIdController have error: " + error.message
       );
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: "Server have error." });
     }
   }
 
   static async addStudentController(req, res) {
     try {
       console.log("\n>>> addStudentController is called.");
-      const { _id, fullName, className, subjects } = req.body;
-      // check input data is empty or not
-      if (!_id.trim() || !fullName.trim() || !className.trim()) {
-        res.status(400).json({ message: "Incorrect student data in body." });
-      } else {
-        // check new student is already exists or not
-        const currentStudent = await studentService.getStudentByIdService(_id);
-        if (!currentStudent) {
-          const newStudent = await studentService.addStudentService(
-            _id,
-            fullName,
-            className,
-            subjects
-          );
-          res.status(201).json(newStudent);
-        } else {
-          // add new student to db
-          res.status(400).json({
-            message: `A student with id ${_id} already exists.`
-          });
-        }
+      const { fullName, className, subjects } = req.body;
+      if (!fullName || !className) {
+        res.status(400).json({ message: "Missing student data in body." });
       }
+      const addedStudent = await studentService.addStudentService(
+        fullName,
+        className,
+        subjects
+      );
+      res.status(201).json(addedStudent);
     } catch (error) {
       console.log("\n>>> addStudentController have error: " + error.message);
-      res.status(500).json({ message: error.message });
-    }
-  }
-
-  static async updateStudentByIdController(req, res) {
-    try {
-      console.log("\n>>> updateStudentController is called.");
-      const _id = req.params.id;
-      const { fullName, className, subjects } = req.body;
-      const currentStudent = await studentService.getStudentByIdService(_id);
-      if (!currentStudent) {
-        res.status(404).json({ message: `No student with ${_id} found.` });
-      } else {
-        const updatedStudent = await studentService.updateStudentByIdService(
-          _id,
-          fullName,
-          className,
-          subjects
-        );
-        res.status(200).json(updatedStudent);
-      }
-    } catch (error) {
-      console.log("\n>>> updateStudentController have error: " + error.message);
-      res.status(500).json({ message: error.message });
-    }
-  }
-
-  static async deleteStudentByIdController(req, res) {
-    try {
-      console.log("\n>>> deleteStudentByIdController is called.");
-      const _id = req.params.id;
-      const currentStudent = await studentService.getStudentByIdService(_id);
-      if (!currentStudent) {
-        res.status(404).json({ message: `No student with ${_id} found.` });
-      } else {
-        const deletedStudent = await studentService.deleteStudentByIdService(
-          _id
-        );
-        res.status(200).json(deletedStudent);
-      }
-    } catch (error) {
-      console.log(
-        "\n>>> deleteStudentByIdController have error: " + error.message
-      );
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: "Server have error." });
     }
   }
 }
@@ -117,5 +61,5 @@ module.exports = StudentController;
 //   console.log("\n>>> functionName is called.");
 // } catch (error) {
 //   console.log("\n>>> functionName have error: " + error.message);
-//   res.status(500).json({ message: error.message });
+//   res.status(500).json({ message: "Server have error." });
 // }

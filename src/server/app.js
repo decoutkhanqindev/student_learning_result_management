672d1de6747fa225bfda265c5d21ddf0843372ac
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-01-29 22:08:40 +0700

    update project

--- src/server/app.js
+++ src/server/app.js
@@ -11,6 +11,7 @@ const env = process.env;
 const PORT = env.PORT || 8080;
 const MONGO_DB_URI = env.MONGO_DB_URI;
 
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(apiRouter);
